996b6311aeb8af1903cc2b88ce4c3b88b8228ea5
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-10-04 01:58:35 +0700

    fillter

--- BE/src/modules/products/products.service.ts
+++ BE/src/modules/products/products.service.ts
@@ -5,6 +5,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import aqp from 'api-query-params';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class ProductsService {
@@ -24,9 +25,43 @@ export class ProductsService {
     delete filter.current;
     delete filter.pageSize;
 
+    // 👉 Convert category
+    if (filter.category) {
+      if (typeof filter.category === 'string') {
+        // Nếu có nhiều id (cách nhau dấu phẩy)
+        if (filter.category.includes(',')) {
+          filter.category = {
+            $in: filter.category
+              .split(',')
+              .map((id) => new mongoose.Types.ObjectId(id)),
+          };
+        } else {
+          // 1 id duy nhất
+          filter.category = new mongoose.Types.ObjectId(filter.category);
+        }
+      }
+    }
+
+    // 👉 Convert price filter
+    if (filter.price) {
+      const priceFilter: any = {};
+      if (filter.price.gte !== undefined) {
+        priceFilter.$gte = Number(filter.price.gte);
+      }
+      if (filter.price.lte !== undefined) {
+        priceFilter.$lte = Number(filter.price.lte);
+      }
+      if (Object.keys(priceFilter).length > 0) {
+        filter.price = priceFilter;
+      } else {
+        delete filter.price;
+      }
+    }
+
+    console.log('👉 Final filter:', JSON.stringify(filter));
+
     const safePage = Math.max(1, Number(currentPage) || 1);
     const safeLimit = Math.max(1, Number(limit) || 20);
-
     const offset = (safePage - 1) * safeLimit;
 
     const totalItems = await this.productModel.countDocuments(filter);
