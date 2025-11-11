26c2dba36f7d1631426752951065e54ccacd670e
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-11-12 04:29:33 +0700

    Promo

--- FE/src/components/carousel/carousel.tsx
+++ FE/src/components/carousel/carousel.tsx
@@ -2,12 +2,13 @@
 
 import { Carousel } from "antd";
 import Image from "next/image";
-
+import Link from "next/link";
 
 type Slide = {
   id: number;
   title: string;
   image: string;
+  link: string;
 };
 
 const slides: Slide[] = [
@@ -15,16 +16,19 @@ const slides: Slide[] = [
     id: 1,
     title: "",
     image: "/images/banners/banner6.png",
+    link: "/product-detail/69079cbdd5402175fadf4838",
   },
   {
     id: 2,
     title: "",
-    image: "/images/banners/banner7.png",
+    image: "/images/banners/EdraBanner.jpg",
+    link: "/productsList?category=69079cbcd5402175fadf481f&sort=-sold",
   },
   {
     id: 3,
     title: "",
     image: "/images/banners/banner5.jpg",
+    link: "/product-detail/69079cbdd5402175fadf4832",
   },
 ];
 
@@ -46,40 +50,42 @@ const MainCarousel: React.FC = () => {
       <Carousel autoplay arrows fade adaptiveHeight>
         {slides.map((slide) => (
           <div key={slide.id}>
-            <div
-              style={{
-                width: "100%",
-                height: "700px",
-                position: "relative",
-                backgroundColor: "#000",
-              }}
-            >
-              <Image
-                src={slide.image}
-                alt={slide.title}
-                fill
-                style={{
-                  objectFit: "fill",
-                }}
-                sizes="100vw"
-              />
-
+            <Link href={slide.link}>
               <div
                 style={{
-                  position: "absolute",
-                  inset: 0,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  color: "#fff",
-                  fontSize: 32,
-                  fontWeight: 700,
-                  textShadow: "0 2px 8px rgba(0,0,0,0.6)",
+                  width: "100%",
+                  height: "700px",
+                  position: "relative",
+                  backgroundColor: "#000",
                 }}
               >
-                {slide.title}
+                <Image
+                  src={slide.image}
+                  alt={slide.title}
+                  fill
+                  style={{
+                    objectFit: "fill",
+                  }}
+                  sizes="100vw"
+                />
+
+                <div
+                  style={{
+                    position: "absolute",
+                    inset: 0,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#fff",
+                    fontSize: 32,
+                    fontWeight: 700,
+                    textShadow: "0 2px 8px rgba(0,0,0,0.6)",
+                  }}
+                >
+                  {slide.title}
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </Carousel>
