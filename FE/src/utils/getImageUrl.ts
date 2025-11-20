75392b2846f4a86ca7572bfb8c6e71a5fcd2b30d
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-11-21 03:45:41 +0700

    fix localhost img

--- FE/src/utils/getImageUrl.ts
+++ FE/src/utils/getImageUrl.ts
@@ -1,5 +1,13 @@
 export const getImageUrl = (url?: string) => {
   if (!url) return "";
-  if (url.startsWith("http://") || url.startsWith("https://")) return url;
+
+  if (url.startsWith("http://") || url.startsWith("https://")) {
+    return url;
+  }
+
+  if (url.startsWith("/slider") || url.startsWith("/thumbnails")) {
+    return `${process.env.NEXT_PUBLIC_BACKEND_URL}/images${url}`;
+  }
+
   return `${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`;
 };
