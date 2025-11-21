234014f105729f7ab49aa07edcfa3d4eb6a01474
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-11-21 23:45:20 +0700

    fix img upddate

--- FE/src/utils/getImageUrl.ts
+++ FE/src/utils/getImageUrl.ts
@@ -3,12 +3,9 @@ const BACKEND_URL = "https://next-nest-ecommerce.onrender.com";
 export const getImageUrl = (url?: string) => {
   if (!url) return "";
 
-  // Nếu URL full thì trả về luôn
   if (url.startsWith("http://") || url.startsWith("https://")) {
     return url;
   }
 
-  // Mặc định DB lưu dạng /slider/xxx hoặc /thumbnails/xxx
-  // => Tất cả ảnh đều phải map về /images/*
   return `${BACKEND_URL}/images${url}`;
 };
